8ce7ca4934d3e631c4a3ffcbb3be0c9b1b28b007
Tien Phan <tienphan1989@gmail.com>
2020-05-06 22:16:33 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,14 +1,11 @@
 def reverse_each_word(string)
   new_array = []
-  string.each { |word| new_array.push(string.split(' ').reverse()
-  
-  original_array = string.split(" ")
-  return_array = []
-  original_array.each do|string|
-    return_array.push(string.reverse)
-  end
-  return_array.join(" ")
+  string.each { |word| new_array.push(word.split(' ').reverse())}
+  new_array.join(" ")
 end
+  
+  
+
 
 
   
