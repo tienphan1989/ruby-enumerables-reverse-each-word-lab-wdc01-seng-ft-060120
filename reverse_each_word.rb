88b7c747c23e321c9b1fbbedc20e69de7e35ebaa
Tien Phan <tienphan1989@gmail.com>
2020-05-06 22:12:57 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,8 +1,13 @@
 def reverse_each_word(string)
   string.each { |word| string.split(' ').reverse()
   
+  original_array = string.split(" ")
+  return_array = []
+  original_array.each do|string|
+    return_array << string.reverse
+  end
+  return_array.join(" ")
 end
 
-reverse_each_word("Hello there")
 
   
